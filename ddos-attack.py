6c389d934d0ac43bda55b80347cb0381cd4c21bb
AlexxLy <80522799+AlexxLy@users.noreply.github.com>
2021-08-02 15:20:40 +0200

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -21,9 +21,9 @@ os.system("clear")
 os.system("figlet DDos Attack")
 print
 print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "You Tube : https://www.youtube.com/c/AlexxModder"
+print "github   : https://github.com/AlexxLy"
+print "Instagram : https://www.instagram.com/dcs.alexx"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
